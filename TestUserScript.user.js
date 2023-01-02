10617589b8e1be18038295946288da0a6d96e3e5
jhudora <32663620+jhudora@users.noreply.github.com>
2023-01-02 20:27:24 +1300

    Create TestUserScript.user.js

---
+++ TestUserScript.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         Test Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        *
+// ==/UserScript==
+
+console.log("TEST");
