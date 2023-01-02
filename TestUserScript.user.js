f7d6719ed2e9871331b111b0feaa15e5a2e5e2f8
jhudora <32663620+jhudora@users.noreply.github.com>
2023-01-02 20:45:09 +1300

    Update TestUserScript.user.js

--- TestUserScript.user.js
+++ TestUserScript.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Test Userscript
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.1.1
 // @description  try to take over the world!
 // @author       You
 // @match        *
