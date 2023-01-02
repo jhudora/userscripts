e97ed718584de17e83ac761df930be204c9c4889
jhudora <32663620+jhudora@users.noreply.github.com>
2023-01-02 20:58:28 +1300

    Update TestUserScript.user.js

--- TestUserScript.user.js
+++ TestUserScript.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Test Userscript
 // @namespace    http://tampermonkey.net/
-// @version      0.1.2
+// @version      0.1.3
 // @description  try to take over the world!
 // @author       You
 // @match        *
@@ -9,4 +9,4 @@
 // @downloadURL  https://github.com/jhudora/userscripts/raw/main/TestUserScript.user.js
 // ==/UserScript==
 
-console.log("TEST FUCKING 3");
+console.log("TEST FUCKING 4");
